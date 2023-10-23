31fc220b2623628efc0b513aadb612dce13f3728
ArseniyBeglov <HUDAIDOTT@yandex.ru>
2023-10-23 15:50:55 +0300

    add url session

--- project_hw_ios/ViewController.swift
+++ project_hw_ios/ViewController.swift
@@ -6,15 +6,43 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
-            super.viewDidLoad()
-            view.backgroundColor = .systemRed
-            // Do any additional setup after loading the view.
-        }
-
-
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+        let url: URL = URL(string: "https://api.thedogapi.com/v1/breeds")!
+        URLSession.shared.dataTask(with: url, completionHandler: { data, response, error in
+            guard let data = data,
+                  error == nil
+            else{
+                return
+            }
+            let decoder = JSONDecoder()
+  
+            let model = try! decoder.decode([DogDTO].self, from: data)
+            print(model)
+        }).resume()
+    }
+    
 }
+
+struct DogDTO: Decodable {
+    let id: Int
+    let name: String
+    let bredFor: String?
+    let breedGroup: String?
+    let referenceImageId: String
+    private enum CodingKeys: String, CodingKey {
+        case id
+        case name
+        case bredFor = "bred_for"
+        case breedGroup = "breed_group"
+        case referenceImageId = "reference_image_id"
+    }
+}
+
+
 
